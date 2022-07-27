b452d756df471747de22647a1d009e92da17328b
Salman H <Salman0112358@outlook.com>
2022-07-27 14:44:55 +0100

    saving

--- src/1-async-return.ts
+++ src/1-async-return.ts
@@ -6,7 +6,14 @@ async function promiseToGreet(): Promise<void> {
   console.log("hello world"!);
 }
 
-greet();
+async function promiseToAdd(a: number, b: number) {
+  console.log('chaning async functions')
+  return a + b;
+}
+
+//greet();
 // promiseToGreet()
 // greet().then(() => console.log("CANNOT do this"));
 // promiseToGreet().then(() => console.log("CAN do this"));
+
+promiseToGreet().then(() => promiseToAdd(5,4) )
